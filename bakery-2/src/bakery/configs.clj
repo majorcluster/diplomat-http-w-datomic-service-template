d7ea9edca963ce63c967c889d31a049bd9525e30
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-03-25 00:59:03 -0300

    removing generated folders

--- bakery-2/src/bakery/configs.clj
+++
@@ -1,8 +0,0 @@
-(ns bakery.configs
-  (:require [outpace.config :refer [defconfig]]))
-
-(defconfig env)
-
-(defn env-test?
-  []
-  (= "test" env))
--- bakery/src/bakery/ports/http/routes/utils.clj
+++
@@ -1,4 +0,0 @@
-(ns bakery.ports.http.routes.utils)
-
-(def json-header
-  {"Content-Type" "application/json"})
