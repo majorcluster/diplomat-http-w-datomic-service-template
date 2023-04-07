e6ffcbf76074188ef9d7e7b3973acf7deca46b83
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-04-06 23:34:04 -0300

    Adding auto-deployment script

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject org.clojars.majorcluster/lein-template.diplomat-http-w-datomic-service "0.2.2"
+(defproject org.clojars.majorcluster/lein-template.diplomat-http-w-datomic-service "0.2.3"
   :description "Diplomat architecture-pedestal styled template with datomic db for leiningen generation"
   :url "https://github.com/majorcluster/diplomat-http-w-datomic-service-template"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]]
   :eval-in-leiningen true)
